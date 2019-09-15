43a39adc01425054e8cf07b4d7de397a01834d78
Andrey <andrey.korolko@mail.ru>
2019-09-15 17:38:47 +0300

    update specification: send params as json

--- spec/controllers/api/v1/equations_controller_spec.rb
+++ spec/controllers/api/v1/equations_controller_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 describe Api::V1::EquationsController do
   describe 'POST #linear' do
     it 'with valid params' do
-      post :linear, params: { coefficients: [2, -10] }
+      post :linear, params: { coefficients: [2, -10] }, as: :json
 
       expect(response).to have_http_status :ok
-      expect(response.body).to eq({ root: [5] }.to_json)
+      expect(response.body).to eq({ root: [5.0] }.to_json)
     end
 
     context 'with invalid params' do
@@ -18,21 +18,21 @@ describe Api::V1::EquationsController do
       end
 
       it 'when wrong type of coefficients' do
-        post :linear, params: { coefficients: 'coefficients' }
+        post :linear, params: { coefficients: 'coefficients' }, as: :json
 
         expect(response).to have_http_status :ok
         expect(response.body).to eq({ error: 'wrong type of coefficients (given String, expected Array)' }.to_json)
       end
 
       it 'when wrong number of coefficients' do
-        post :linear, params: { coefficients: [1, 2, 3] }
+        post :linear, params: { coefficients: [1, 2, 3] }, as: :json
 
         expect(response).to have_http_status :ok
         expect(response.body).to eq({ error: 'wrong number of coefficients (given 3, expected 2)' }.to_json)
       end
 
       it 'when one of coefficients is not Numeric' do
-        post :quadratic, params: { coefficients: [1, 'String', 2] }
+        post :quadratic, params: { coefficients: [1, 'String', 2] }, as: :json
 
         expect(response).to have_http_status :ok
         expect(response.body).to eq({ error: 'wrong type of coefficient' }.to_json)
@@ -42,10 +42,10 @@ describe Api::V1::EquationsController do
 
   describe 'POST #quadratic' do
     it 'with valid params' do
-      post :quadratic, params: { coefficients: [1, -8, 12] }
+      post :quadratic, params: { coefficients: [1, -8, 12] }, as: :json
 
       expect(response).to have_http_status :ok
-      expect(response.body).to eq({ roots: [6, 2] }.to_json)
+      expect(response.body).to eq({ roots: [6.0, 2.0] }.to_json)
     end
 
     context 'with invalid params' do
@@ -57,28 +57,28 @@ describe Api::V1::EquationsController do
       end
 
       it 'when wrong type of coefficients' do
-        post :quadratic, params: { coefficients: 'coefficients' }
+        post :quadratic, params: { coefficients: 'coefficients' }, as: :json
 
         expect(response).to have_http_status :ok
         expect(response.body).to eq({ error: 'wrong type of coefficients (given String, expected Array)' }.to_json)
       end
 
       it 'when wrong number of coefficients' do
-        post :quadratic, params: { coefficients: [1, 2] }
+        post :quadratic, params: { coefficients: [1, 2] }, as: :json
 
         expect(response).to have_http_status :ok
         expect(response.body).to eq({ error: 'wrong number of coefficients (given 2, expected 3)' }.to_json)
       end
 
       it 'when wrong value of `a` coefficient' do
-        post :quadratic, params: { coefficients: [0, 2, 3] }
+        post :quadratic, params: { coefficients: [0, 2, 3] }, as: :json
 
         expect(response).to have_http_status :ok
         expect(response.body).to eq({ error: "wrong value of `a` coefficient (can't be 0)" }.to_json)
       end
 
       it 'when one of coefficients is not Numeric' do
-        post :quadratic, params: { coefficients: [1, 'String', 2] }
+        post :quadratic, params: { coefficients: [1, 'String', 2] }, as: :json
 
         expect(response).to have_http_status :ok
         expect(response.body).to eq({ error: 'wrong type of coefficient' }.to_json)
