04260dea6a0e501fbf6c51da467271b80c0e2359
Andrey <andrey.korolko@mail.ru>
2019-09-15 23:50:49 +0300

    add cors vaidation

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,6 +1,17 @@
 module Api
   module V1
     class ApiController < ActionController::API
+      before_action :cors_set_access_control_headers
+
+      def cors_preflight_check
+        head :ok
+      end
+
+      def cors_set_access_control_headers
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
+      end
     end
   end
 end
